749296b79e0ec2c3263e3b44f0b9666fe95ce1dd
jgoerzen <jgoerzen>
2002-06-22 05:10:49 +0100

    /head: changeset 40
    Added

---
+++ head/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2.2
+
+# $Id: setup.py,v 1.1 2002/06/21 18:10:49 jgoerzen Exp $
+
+# IMAP synchronization
+# Module: installer
+# COPYRIGHT #
+# Copyright (C) 2002 John Goerzen
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# END OF COPYRIGHT #
+
+
+from distutils.core import setup
+import offlineimap.version
+
+setup(name = "offlineimap",
+      version = offlineimap.version.versionstr,
+      description = offlineimap.version.description,
+      author = offlineimap.version.author,
+      author_email = offlineimap.version.author_email,
+      url = offlineimap.version.homepage,
+      packages = ['offlineimap', 'offlineimap.folder',
+                  'offlineimap.repository', 'offlineimap.ui'],
+      scripts = ['offlineimap.py'],
+      license = offlineimap.version.license
+)
+
