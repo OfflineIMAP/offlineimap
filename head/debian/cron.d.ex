872fce5502b6233276fc20df0cd3574e901e6a06
jgoerzen <jgoerzen>
2002-06-22 06:55:24 +0100

    /head: changeset 45
    Updated

---
+++ head/debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the offlineimap package
+#
+0 4	* * *	root	offlineimap_maintenance
---
+++ head/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(offlineimap):needs=X11|text|vc|wm section=Apps/see-menu-manual\
+  title="offlineimap" command="/usr/bin/offlineimap"
