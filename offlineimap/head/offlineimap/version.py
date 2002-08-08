d9836ef7912e01ccf6bbcb5eb06d4faed96a60a2
jgoerzen <jgoerzen>
2002-08-08 21:21:56 +0100

    /offlineimap/head: changeset 221
    Readying 3.2.3. Updated docs, version, etc.

--- offlineimap/head/offlineimap/version.py
+++ offlineimap/head/offlineimap/version.py
@@ -1,8 +1,8 @@
 productname = 'OfflineIMAP'
 versionstr = "3.2.2"
-revno = long('$Rev: 215 $'[6:-2])
+revno = long('$Rev: 221 $'[6:-2])
 revstr = "Rev %d" % revno
-datestr = '$Date: 2002-08-07 20:28:34 -0500 (Wed, 07 Aug 2002) $'
+datestr = '$Date: 2002-08-08 10:21:56 -0500 (Thu, 08 Aug 2002) $'
 
 
 versionlist = versionstr.split(".")
@@ -41,67 +41,71 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA"""
 
 cmdhelp = """
        offlineimap [ -1 ] [ -P profiledir ] [ -a accountlist ]  [
-       -c configfile ] [ -d ] [ -o ] [ -u interface ]
+       -c configfile  ] [ -d debugtype[,debugtype...]  ] [ -o ] [
+       -u interface ]
 
        offlineimap -h | --help
 
-       -1     Disable  all  multithreading  operations  and   use
+       -1     Disable   all  multithreading  operations  and  use
               solely a single-thread sync.  This effectively sets
               the maxsyncaccounts and all maxconnections configu-
               ration file variables to 1.
 
        -P profiledir
-              Sets  OfflineIMAP  into  profile mode.  The program
+              Sets OfflineIMAP into profile  mode.   The  program
               will create profiledir (it must not already exist).
               As it runs, Python profiling information about each
-              thread is logged  into  profiledir.   Please  note:
-              This  option is present for debugging and optimiza-
+              thread  is  logged  into  profiledir.  Please note:
+              This option is present for debugging and  optimiza-
               tion only, and should NOT be used unless you have a
-              specific  reason  to  do so.  It will significantly
-              slow program performance, may  reduce  reliability,
-              and  can  generate  huge amounts of data.  You must
+              specific reason to do so.   It  will  significantly
+              slow  program  performance, may reduce reliability,
+              and can generate huge amounts of  data.   You  must
               use the -1 option when you use -P.
 
 
        -a accountlist
-              Overrides the accounts section in the config  file.
-              Lets  you  specify  a  particular account or set of
-              accounts to sync without having to edit the  config
+              Overrides  the accounts section in the config file.
+              Lets you specify a particular  account  or  set  of
+              accounts  to sync without having to edit the config
               file.   You  might  use  this  to  exclude  certain
-              accounts, or to sync some accounts  that  you  nor-
+              accounts,  or  to  sync some accounts that you nor-
               mally prefer not to.
 
        -c configfile
-              Specifies  a  configuration  file to use in lieu of
+              Specifies a configuration file to use  in  lieu  of
               the default, ~/.offlineimaprc.
 
-       -d     Enables IMAP protocol stream and parsing debugging.
-              This  is  useful  if you are trying to track down a
-              malfunction or figure out what is  going  on  under
-              the  hood.   I suggest that you use this with -1 in
-              order to make the results more sensible.  Note that
-              this  output  will  contain  full  IMAP protocol in
-              plain text, including passwords, so  take  care  to
-              remove  that from the debugging output before send-
-              ing it to anyone else.
+       -d debugtype[,debugtype...]
+              Enables  debugging for OfflineIMAP.  This is useful
+              if you are trying to track down  a  malfunction  or
+              figure out what is going on under the hood.  I sug-
+              gest that you use this with -1 in order to make the
+              results more sensible.
 
-       -o     Run only once, ignoring any autorefresh setting  in
+              -d  now  requires one or more debugtypes, separated
+              by commas.   These  define  what  exactly  will  be
+              debugged,  and so far include two options: imap and
+              maildir.  The imap option will enable IMAP protocol
+              stream and parsing debugging.  Note that the output
+              may contain passwords, so take care to remove  that
+              from the debugging output before sending it to any-
+              one else.  The maildir option will enable debugging
+              for certain Maildir operations.
+
+       -o     Run  only once, ignoring any autorefresh setting in
               the config file.
 
        -h, --help
               Show summary of options.
 
        -u interface
-              Specifies  an  alternative user interface module to
-              use.  This overrides the default specified  in  the
-              configuration  file.  The UI specified with -u will
-              be forced to be used, even if its isuable()  method
-              states  that  it  cannot  be.  Use this option with
-              care.
+              Specifies an alternative user interface  module  to
+              use.   This  overrides the default specified in the
+              configuration file.  The UI specified with -u  will
+              be  forced to be used, even if its isuable() method
+              states that it cannot be.   Use  this  option  with
+              care.   The  pre-defined  options are listed in the
+              USER INTERFACES section.
 
-              The pre-defined options are  Tk.TKUI  (a  graphical
-              interface), TTY.TTYUI (a text-mode interface), Non-
-              interactive.Basic (a non-interactive mode  suitable
-              for  cronning),  and  Noninteractive.Quiet  (a mode
-              that generates no output except for errors).
 """
