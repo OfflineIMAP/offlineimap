afba647f620d33d71d04561160b9d6e75fa7e387
jgoerzen <jgoerzen>
2003-04-29 22:30:26 +0100

    /offlineimap/head: changeset 467
    When sep was /, the new Maildir support code would recursively try to
    scan ., resulting in huge paths and an eventual crash. Fixed with a
    one-line patch to Maildir.py. Closes: [complete.org #60] Sergei, The
    below diff is going into 3.99.16. You can apply it to 3.99.15 and it
    should work for you now. Please let me know. (Ignore any patch errors
    for debian/changelog). Thanks for the report.

--- offlineimap/head/offlineimap/repository/Maildir.py
+++ offlineimap/head/offlineimap/repository/Maildir.py
@@ -132,7 +132,7 @@ class MaildirRepository(BaseRepository):
 
                 retval.append(folder.Maildir.MaildirFolder(self.root, foldername,
                                                            self.getsep(), self, self.accountname))
-            if self.getsep() == '/':
+            if self.getsep() == '/' and dirname != '.':
                 # Check sub-directories for folders.
                 retval.extend(self._getfolders_scandir(root, foldername))
         self.debug("_GETFOLDERS_SCANDIR RETURNING %s" % \
