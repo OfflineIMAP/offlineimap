65a6b8aa81b605d05b4dd58c04c9e146a07e4022
David Favro <offlineimap@meta-dynamic.com>
2007-03-16 03:45:28 +0100

    Removed copyright notice; assigning copyright to John Goerzen

--- offlineimap/ui/UIBase.py
+++ offlineimap/ui/UIBase.py
@@ -2,8 +2,6 @@
 # Copyright (C) 2002 John Goerzen
 # <jgoerzen@complete.org>
 #
-# Portions Copyright (C) 2007 David Favro <offlineimap@meta-dynamic.com>
-#
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation; either version 2 of the License, or
