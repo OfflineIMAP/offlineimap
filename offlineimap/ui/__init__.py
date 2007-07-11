84d166248248d08e5becb7af2018431130d15598
John Goerzen <jgoerzen@complete.org>
2007-07-11 02:50:30 +0100

    Update changelog

--- offlineimap/ui/__init__.py
+++ offlineimap/ui/__init__.py
@@ -17,7 +17,7 @@
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
 
-import UIBase, Blinkenlights, transmitter
+import UIBase, Blinkenlights
 try:
     import TTY
 except ImportError:
