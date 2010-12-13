OfflineIMAP/offlineimap

commit 9239a2d3268e155d13c972808ea7d07dca87d0ce

Bugfix patch for offlineimap
Hello John,

i fixed some tiny bugs in offlineimap, mainly just for myself. They are
more dirty fixes than real bugfixes since I'm missing the deeper insight
into the code.
Especially the first one for Curses.py is very dirty and breaks the
scaling of the interface when the terminal size changes, but at least
the terminal is in proper state after exiting offlineimap.

In the order of appearance in the patchfile:
1. 'fixes' terminal breakage on quit of curses interface in python 2.6
to 2.6.5 (fixed since 2.6.6 http://bugs.python.org/issue7567)
2. fixes netrc password authentication
3. fixes user name querying from netrc

The patch is made for git revision 6b1cb5e036f144a6609d

Thanks a lot for the great application!

Best regards,
buergi

offlineimap/imapserver.py
@@ -176,7 +176,7 @@ class IMAPServer:
         challenge = response.strip()
         ui.debug('imap', 'md5handler: got challenge %s' % challenge)
 
-        passwd = self.getpassword()
+        passwd = self.repos.getpassword()
         retval = self.username + ' ' + hmac.new(passwd, challenge).hexdigest()
         ui.debug('imap', 'md5handler: returning %s' % retval)
         return retval
@@ -184,7 +184,7 @@ class IMAPServer:
     def plainauth(self, imapobj):
         UIBase.getglobalui().debug('imap',
                                    'Attempting plain authentication')
-        imapobj.login(self.username, self.getpassword())
+        imapobj.login(self.username, self.repos.getpassword())
 
     def gssauth(self, response):
         data = base64.b64encode(response)

offlineimap/repository/IMAP.py
@@ -109,7 +109,7 @@ class IMAPRepository(BaseRepository):
             return user
 
         try:
-            netrcentry = netrc.netrc().authentificator(self.gethost())
+            netrcentry = netrc.netrc().authenticators(self.gethost())
         except IOError, inst:
             if inst.errno != errno.ENOENT:
                 raise
@@ -118,7 +118,7 @@ class IMAPRepository(BaseRepository):
                 return netrcentry[0]
 
         try:
-            netrcentry = netrc.netrc('/etc/netrc').authentificator(self.gethost())
+            netrcentry = netrc.netrc('/etc/netrc').authenticators(self.gethost())
         except IOError, inst:
             if inst.errno != errno.ENOENT:
                 raise

offlineimap/ui/Curses.py
@@ -122,6 +122,8 @@ class CursesUtil:
         del self.stdscr
 
     def reset(self):
+        # dirty walkaround for bug http://bugs.python.org/issue7567 in python 2.6 to 2.6.5 (fixed since #83743)
+        if (sys.version_info[0:3] >= (2,6) and  sys.version_info[0:3] <= (2,6,5)): return
         self.stop()
         self.start()
 

commit b853b58578df9315b762716490edfcbec790fdbf

Unbreak getting password from UI
Commit 9239a2d3268e155d13c9 broke getting the password from the UI. This
unbreaks the change and adds some extended documentation and cleanups in
the functino en-passent.

Signed-off-by: Sebastian Spaeth <Sebastian@SSpaeth.de>
Signed-off-by: Nicolas Sebrecht <nicolas.s-dev@laposte.net>

offlineimap/imapserver.py
@@ -176,7 +176,7 @@ class IMAPServer:
         challenge = response.strip()
         ui.debug('imap', 'md5handler: got challenge %s' % challenge)
 
-        passwd = self.repos.getpassword()
+        passwd = self.getpassword()
         retval = self.username + ' ' + hmac.new(passwd, challenge).hexdigest()
         ui.debug('imap', 'md5handler: returning %s' % retval)
         return retval
@@ -184,7 +184,7 @@ class IMAPServer:
     def plainauth(self, imapobj):
         UIBase.getglobalui().debug('imap',
                                    'Attempting plain authentication')
-        imapobj.login(self.username, self.repos.getpassword())
+        imapobj.login(self.username, self.getpassword())
 
     def gssauth(self, response):
         data = base64.b64encode(response)

offlineimap/repository/IMAP.py
@@ -152,24 +152,35 @@ class IMAPRepository(BaseRepository):
         return self.getconfboolean('expunge', 1)
 
     def getpassword(self):
-        passwd = None
-        localeval = self.localeval
+        """Return the IMAP password for this repository.
 
-        if self.config.has_option(self.getsection(), 'remotepasseval'):
-            passwd = self.getconf('remotepasseval')
+        It tries to get passwords in the following order:
+
+        1. evaluate Repository 'remotepasseval'
+        2. read password from Repository 'remotepass'
+        3. read password from file specified in Repository 'remotepassfile'
+        4. read password from ~/.netrc
+        5. read password from /etc/netrc
+
+        On success we return the password.
+        If all strategies fail we return None.
+        """
+        # 1. evaluate Repository 'remotepasseval'
+        passwd = self.getconf('remotepasseval', None)
         if passwd != None:
-            return localeval.eval(passwd)
-
+            return self.localeval.eval(passwd)
+        # 2. read password from Repository 'remotepass'
         password = self.getconf('remotepass', None)
         if password != None:
             return password
+        # 3. read password from file specified in Repository 'remotepassfile'
         passfile = self.getconf('remotepassfile', None)
         if passfile != None:
             fd = open(os.path.expanduser(passfile))
             password = fd.readline().strip()
             fd.close()
             return password
-
+        # 4. read password from ~/.netrc
         try:
             netrcentry = netrc.netrc().authenticators(self.gethost())
         except IOError, inst:
@@ -180,6 +191,7 @@ class IMAPRepository(BaseRepository):
                 user = self.getconf('remoteuser')
                 if user == None or user == netrcentry[0]:
                     return netrcentry[2]
+        # 5. read password from /etc/netrc
         try:
             netrcentry = netrc.netrc('/etc/netrc').authenticators(self.gethost())
         except IOError, inst:
@@ -190,7 +202,9 @@ class IMAPRepository(BaseRepository):
                 user = self.getconf('remoteuser')
                 if user == None or user == netrcentry[0]:
                     return netrcentry[2]
+        # no strategy yielded a password!
         return None
+
 
     def getfolder(self, foldername):
         return self.getfoldertype()(self.imapserver, foldername,