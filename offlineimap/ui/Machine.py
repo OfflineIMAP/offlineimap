93f4a19778b1a6c1977cc784a5b2706dfd32e1f4
Sebastian Spaeth <Sebastian@SSpaeth.de>
2012-02-05 12:06:18 +0100

    python3: urlencode is in a different module
    import urlencode from urllib.parse if neeeded for python3.
    
    Signed-off-by: Sebastian Spaeth <Sebastian@SSpaeth.de>

--- offlineimap/ui/Machine.py
+++ offlineimap/ui/Machine.py
@@ -13,7 +13,10 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
-from urllib import urlencode
+try:
+    from urllib import urlencode
+except ImportError: # python3
+    from urllib.parse import urlencode
 import sys
 import time
 import logging
