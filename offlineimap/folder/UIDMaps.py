d45872e59cfed26b779544a1dec35313cf70a2ac
Dmitrijs Ledkovs <xnox@debian.org>
2012-06-05 01:45:10 +0100

    Add missing OfflineImapError import in folder/UIDMaps.py
    Bug-Debian: http://bugs.debian.org/671279
    Reported-by: Ansgar Burchardt <ansgar@debian.org>
    Signed-off-by: Dmitrijs Ledkovs <xnox@debian.org>
    Signed-off-by: Nicolas Sebrecht <nicolas.s-dev@laposte.net>

--- offlineimap/folder/UIDMaps.py
+++ offlineimap/folder/UIDMaps.py
@@ -15,6 +15,7 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 from threading import Lock
+from offlineimap import OfflineImapError
 from .IMAP import IMAPFolder
 import os.path
 
