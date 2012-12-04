39a70820eea700914bcdf04615f156358746a21a
Michael Vogt <mvo@ubuntu.com>
2012-12-04 19:28:14 +0100

    b/offlineimap/utils.py: its misc utils, maybe it should be something like dist.py or distro.py?

--- offlineimap/utils.py
+++ offlineimap/utils.py
@@ -1,4 +1,4 @@
-# imaplib utilities
+# misc utilities
 # Copyright (C) 2012 Michael Vogt <mvo@debian.org>
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
