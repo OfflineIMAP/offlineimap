OfflineIMAP/offlineimap

commit af640208e18e9f7997ba37587302605af0c95022

make XOAUTH2 configuration option optional
Signed-off-by: Nicolas Sebrecht <nicolas.s-dev@laposte.net>

offlineimap/repository/Gmail.py
@@ -56,12 +56,12 @@ class GmailRepository(IMAPRepository):
 
         Gmail implementation first checks for the usual IMAP settings
         and falls back to imap.gmail.com if not specified."""
-        try:
-            return super(GmailRepository, self).getoauth2_request_url()
-        except OfflineImapError:
-            # nothing was configured, cache and return hardcoded one
+
+        url = super(GmailRepository, self).getoauth2_request_url()
+        if url is None:
+            # Nothing was configured, cache and return hardcoded one.
             self._oauth2_request_url = GmailRepository.OAUTH2_URL
-            return self._oauth2_request_url
+        return self._oauth2_request_url
 
     def getport(self):
         return GmailRepository.PORT

offlineimap/repository/IMAP.py
@@ -259,7 +259,7 @@ class IMAPRepository(BaseRepository):
         return [f.strip().lower() for f in value.split(',') if f]
 
     def getoauth2_request_url(self):
-        if self._oauth2_request_url:  # use cached value if possible
+        if self._oauth2_request_url:  # Use cached value if possible.
             return self._oauth2_request_url
 
         oauth2_request_url = self.getconf('oauth2_request_url', None)
@@ -267,11 +267,8 @@ class IMAPRepository(BaseRepository):
             self._oauth2_request_url = oauth2_request_url
             return self._oauth2_request_url
 
-        # no success
-        raise OfflineImapError("No remote oauth2_request_url for repository "\
-                                   "'%s' specified." % self,
-                               OfflineImapError.ERROR.REPO)
-        return self.getconf('oauth2_request_url', None)
+        #raise OfflineImapError("No remote oauth2_request_url for repository "
+            #"'%s' specified."% self, OfflineImapError.ERROR.REPO)
 
     def getoauth2_refresh_token(self):
         return self.getconf('oauth2_refresh_token', None)

commit 9bb27de3753433fa340a99e22c8d574b60b44024

do use the XOAUTH2 url if configured
fix regression introduced by af640208e18e9f7.

Signed-off-by: Nicolas Sebrecht <nicolas.s-dev@laposte.net>

offlineimap/repository/Gmail.py
@@ -61,6 +61,8 @@ class GmailRepository(IMAPRepository):
         if url is None:
             # Nothing was configured, cache and return hardcoded one.
             self._oauth2_request_url = GmailRepository.OAUTH2_URL
+        else:
+            self._oauth2_request_url = url
         return self._oauth2_request_url
 
     def getport(self):