90244b7fa65eb47fc6545e4c5b05d555f5847100
Nicolas Sebrecht <nicolas.s-dev@laposte.net>
2017-06-30 05:10:14 +0200

    provide more details in error message when SSL fails on non-standard port
    Signed-off-by: Nicolas Sebrecht <nicolas.s-dev@laposte.net>

--- offlineimap/imapserver.py
+++ offlineimap/imapserver.py
@@ -619,7 +619,8 @@ class IMAPServer(object):
                 if self.port != 993:
                     reason = "Could not connect via SSL to host '%s' and non-s"\
                         "tandard ssl port %d configured. Make sure you connect"\
-                        " to the correct port."% (self.hostname, self.port)
+                        " to the correct port. Got: %s"%
+                        (self.hostname, self.port, e)
                 else:
                     reason = "Unknown SSL protocol connecting to host '%s' for "\
                          "repository '%s'. OpenSSL responded:\n%s"\
