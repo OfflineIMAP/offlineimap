3bb404bc6f25066cfbd0f0862195ac3a80b52c42
Martin Di Paola <martinp.dipaola@gmail.com>
2020-04-18 19:04:43 +0000

    Move out pkg attributes from __init__.py
    The setup.py uses the version, author and others attributes as metadata
    for the python package.
    
    The setup read them from offlineimap/__init__.py doing an import of the
    module first.
    
    Unfortunately the import also try to import all the dependencies of
    offlineimap which may not be installed by the time. See #661.
    
    Moving out the attributes in a separated module allows to be imported by
    setup.py whitout needing to import the whole offlineimap.
    
    The import of test.OLItest has the same limitation. In this case the
    import was delayed until the real test case run is executed avoiding
    again loading offlineimap from the begin.
    
    Signed-off-by: Martin Di Paola <martinp.dipaola@gmail.com>

---
+++ offlineimap/version.py
@@ -0,0 +1,13 @@
+__productname__ = 'OfflineIMAP'
+# Expecting trailing "-rcN" or "" for stable releases.
+__version__     = "7.3.3"
+__copyright__   = "Copyright 2002-2020 John Goerzen & contributors"
+__author__      = "John Goerzen"
+__author_email__= "offlineimap-project@lists.alioth.debian.org"
+__description__ = "Disconnected Universal IMAP Mail Synchronization/Reader Support"
+__license__  = "Licensed under the GNU GPL v2 or any later version (with an OpenSSL exception)"
+__bigcopyright__ = """%(__productname__)s %(__version__)s
+  %(__license__)s""" % locals()
+__homepage__ = "http://www.offlineimap.org"
+
+banner = __bigcopyright__
